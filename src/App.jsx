a87bf307226e7f33ab98ab61f6e9b71c45fb9791
Bilous Oleg <olezhka82802@gmail.com>
2026-07-04 14:51:11 +0300

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import Header from './components/Header';
-import styles from './App.module.sass';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Header from './components/Header';
 import Home from './pages/Home';
+import Footer from './components/Footer';
+import styles from './App.module.sass';
 
 function App () {
   return (
@@ -13,6 +14,7 @@ function App () {
             <Route index path='/' element={<Home />} />
           </Routes>
         </main>
+        <Footer />
       </Router>
     </div>
   );
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,27 @@
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import styles from './Footer.module.sass';
+
+function Footer () {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerGroup}>
+        © 2026 Bilous Oleh. All rights reserved.
+      </div>
+      <div className={styles.footerGroup}>
+        <a href='https://github.com/BilousOleg/cinema_manager_project.git'>
+          <GitHubIcon />
+        </a>
+        <a href='https://www.google.com/'>
+          <LinkedInIcon />
+        </a>
+        <a href='https://t.me/bilous_0leh'>
+          <TelegramIcon />
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
