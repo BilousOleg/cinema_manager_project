b5acc83d52e495d09fc6a6b251d9fd1495520269
Bilous Oleg <olezhka82802@gmail.com>
2026-07-04 22:47:06 +0300

    add service

---
+++ src/components/CinemaService/index.jsx
@@ -0,0 +1,11 @@
+import styles from './CinemaService.module.sass';
+
+function CinemaService () {
+  return (
+    <section className={styles.serviceSection}>
+      <h2 className={styles.serviceHeading}>Cinema service</h2>
+    </section>
+  );
+}
+
+export default CinemaService;
