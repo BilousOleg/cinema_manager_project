5d45e49f793e379fee7e70ca584b8d836a9fa4ca
Bilous Oleg <olezhka82802@gmail.com>
2026-07-09 17:54:42 +0300

    Add directors slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import moviesReducer from './slices/moviesSlice';
 import actorsReducer from './slices/actorsSlice';
+import directorsReducer from './slices/directorsSlice';
 
 const store = configureStore({
   reducer: {
     movies: moviesReducer,
     actors: actorsReducer,
+    directors: directorsReducer,
   },
 });
 
---
+++ src/store/slices/directorsSlice.js
@@ -0,0 +1,51 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  directors: [
+    {
+      id: 1,
+      fullName: 'Christopher Nolan',
+      birthDate: '1970-07-30',
+      country: 'United Kingdom',
+      photo:
+        'https://upload.wikimedia.org/wikipedia/commons/9/95/Christopher_Nolan_Cannes_2018.jpg',
+      biography:
+        'British-American film director, producer and screenwriter. Known for large-scale science fiction and psychological thrillers.',
+      movieIds: [1, 3, 5],
+    },
+    {
+      id: 2,
+      fullName: 'Todd Phillips',
+      birthDate: '1970-12-20',
+      country: 'USA',
+      photo:
+        'https://upload.wikimedia.org/wikipedia/commons/7/75/Todd_Phillips_2016.jpg',
+      biography:
+        'American film director, producer and screenwriter. Best known for Joker and The Hangover trilogy.',
+      movieIds: [2],
+    },
+    {
+      id: 3,
+      fullName: 'The Wachowskis',
+      birthDate: '1965-06-21',
+      country: 'USA',
+      photo:
+        'https://upload.wikimedia.org/wikipedia/commons/6/69/Lana_Wachowski_by_Gage_Skidmore.jpg',
+      biography:
+        'American filmmakers best known for creating The Matrix franchise.',
+      movieIds: [4],
+    },
+  ],
+};
+
+const directorsSlice = createSlice({
+  initialState,
+  name: 'directors',
+  reducers: {
+    addDirectors: (state, action) => {},
+  },
+});
+
+const { reducer, actions } = directorsSlice;
+
+export default reducer;
